1874c56d072e3bf5a378d13328700e937e48e7d2
Christian Possidonio <possidonio.ch@gmail.com>
2020-11-28 19:12:27 -0300

    Adjust docker compose

---
+++ docker-commands.sh
@@ -0,0 +1,13 @@
+# Listar containers em uso
+docker ps
+
+# Listar containers existentes
+docker ps -a 
+
+# Ambiente de desenvolvimento
+docker-compose up -d 
+
+# Desfazer ambiente de desenvolvimento
+docker-compose down
+# Remover todas as images.
+docker rmi $(docker images -a -q)

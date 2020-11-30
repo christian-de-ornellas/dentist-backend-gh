45b5dd9b28b6bb90f32b303c2ffb6362c490339a
Christian Possidonio <possidonio.ch@gmail.com>
2020-11-30 12:12:12 -0300

    Search anwers by client and forms

--- src/controllers/ReplysController.ts
+++ src/controllers/ReplysController.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from 'express'
 import { Reply } from '@models/Reply'
+import { Request, Response } from 'express'
 
 class ReplysController {
   public async index(req: Request, res: Response): Promise<Response> {
     try {
-      const replys = await Reply.find().populate('question')
+      const { form, client } = req.query
+      const replys = await Reply.find({ $and: [{ form }, { client }] })
+        .populate('question')
+        .populate('client')
+        .populate('form')
       return res.send(replys)
     } catch (error) {
       return res.status(400).send({ error })
@@ -12,13 +16,7 @@ class ReplysController {
   }
   public async store(req: Request, res: Response): Promise<Response> {
     try {
-      const { question } = req.body
-
-      if (await Reply.findOne({ question })) {
-        return res.status(400).send({ error: 'Reply already exists!' })
-      }
-
-      const newReply = await Reply.create(req.body)
+      await Reply.create(req.body)
       return res.status(201).send({ message: 'Reply created!' })
     } catch (error) {
       return res.status(400).send({ error })

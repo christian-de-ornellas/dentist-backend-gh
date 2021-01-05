04f2809c6576253f7e9244115644f5ae1b71c68b
Christian Possidonio <christian@orientme.com.br>
2021-01-05 15:34:24 -0300

    new config db

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ class App {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
-    const uri = `mongodb://clyviadentist:e77697373@mongo_clyviadentistdb:27017/clyviadentistdb`
+    const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
     mongoose.connect(uri, options)
   }
 

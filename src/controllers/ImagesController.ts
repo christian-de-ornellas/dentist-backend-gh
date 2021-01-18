895e379e0707841fbf41f805beaaf317badba1d0
Christian Possidonio <christian@orientme.com.br>
2021-01-18 14:02:43 -0300

    Enviando um ou mais imagens

--- src/controllers/ImagesController.ts
+++ src/controllers/ImagesController.ts
@@ -1,16 +1,19 @@
+import { Client } from '@models/Client'
 import { NextFunction, Request, Response } from 'express'
 import formidable from 'formidable'
 
 class ImagesController {
   public async store(req: Request, res: Response, next: NextFunction): Promise<Response> {
     try {
-      const form = formidable({ keepExtensions: true, multiples: true, uploadDir: `${process.env.TMP_PATH}`, maxFieldsSize: 10 * 1024 * 1024 }) // maxSize 10MB
+      const form = await formidable({ keepExtensions: true, multiples: true, uploadDir: `${process.env.TMP_PATH}`, maxFieldsSize: 10 * 1024 * 1024 }) // maxSize 10MB
 
-      form.parse(req, (err, fields, files) => {
+      form.parse(req, async (err, fields, files) => {
         if (err) {
           next(err)
           return
         }
+
+        await Client.updateOne({ _id: req.query.clientId }, { $push: { files: files.image } })
 
         res.send({ fields, files })
       })
--- src/models/Client.ts
+++ src/models/Client.ts
@@ -69,6 +69,7 @@ const ClientSchema = new Schema(
       document: String,
       name: String,
     },
+    files: [],
   },
   {
     timestamps: true,

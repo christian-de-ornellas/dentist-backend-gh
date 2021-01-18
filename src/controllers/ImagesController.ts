0c8b48974c27c524cbb96cb879b6caab8a0a95e5
Christian Possidonio <christian@orientme.com.br>
2021-01-18 11:42:37 -0300

    Upload ok

--- src/controllers/ImagesController.ts
+++ src/controllers/ImagesController.ts
@@ -4,12 +4,14 @@ import formidable from 'formidable'
 class ImagesController {
   public async store(req: Request, res: Response, next: NextFunction): Promise<Response> {
     try {
-      const form = formidable({ multiples: true, uploadDir: `${process.env.TMP_PATH}`, maxFieldsSize: 10 * 1024 * 1024 }) // maxSize 10MB
+      const form = formidable({ keepExtensions: true, multiples: true, uploadDir: `${process.env.TMP_PATH}`, maxFieldsSize: 10 * 1024 * 1024 }) // maxSize 10MB
+
       form.parse(req, (err, fields, files) => {
         if (err) {
           next(err)
           return
         }
+
         res.send({ fields, files })
       })
     } catch (error) {
